8655b43e6584ec2b24e16ebd02d034078dec643f
Xu Xiang <xux@geohey.com>
2014-10-28 17:30:40 +0800

    Module find pango and cairo

---
+++ pangocairo_cmake/FindPangoCairo.cmake
@@ -0,0 +1,10 @@
+set(PANGO_CAIRO_INCLUDES
+/usr/local/include/pango-1.0
+/usr/local/include/glib-2.0
+/usr/local/lib/glib-2.0/include
+/usr/local/include/cairo
+  )
+
+set(PANGO_CAIRO_LIBRARIES
+  cairo pango-1.0 pangocairo-1.0 gobject-2.0 glib-2.0
+  )
